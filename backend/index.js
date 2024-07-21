6d283d3cfbd2997d109c3fda8280db6c41803485
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-21 14:38:18 +0500

    Db connection added

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
-console.log("HELLO");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+const server = app.listen(port, () => {
+  console.log(`Server is listining at port ${port}`);
+});
+
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log("DB Connected Successfuly"))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit();
+  });
