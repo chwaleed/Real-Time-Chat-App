9495c77e95541c3d26308ea99918d690edbc94d5
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-23 18:12:12 +0500

    sonner shadcn toaster added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "sonner";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster />
+  </>
+);
