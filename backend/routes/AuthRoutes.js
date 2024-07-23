78d3204dc615d468bdf514d7f9ab68aa78486cbc
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-23 17:47:26 +0500

    AuthRoute Configration Added

---
+++ backend/routes/AuthRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { signup } from "../controllers/AuthController.js";
+
+const authRoutes = Router();
+
+authRoutes.post("/signup", signup);
+
+export default authRoutes;
