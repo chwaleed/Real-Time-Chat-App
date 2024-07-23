201b780ea4d735021e3bd9ca65df723aa8d0d524
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-23 17:44:55 +0500

    sign up controller completed

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -4,7 +4,9 @@ import User from "../models/UserModel";
 const tokenAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (email, password) => {
-  return sign({ email, password }, process.env.JWT_KEY);
+  return sign({ email, password }, process.env.JWT_KEY, {
+    expiresIn: tokenAge,
+  });
 };
 export const signup = async (request, response, next) => {
   try {
@@ -13,6 +15,19 @@ export const signup = async (request, response, next) => {
       return response.status(400).send("Email and Password is required");
     }
     const user = await User.create({ email, password });
+    response.cookie("jwt", createToken(email, user.id), {
+      tokenAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return response.status(201).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+      },
+    });
   } catch (error) {
     console.log({ error });
     return response.status(500).send("Internal Server Error");
