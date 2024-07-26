032f3105f0de1e27debf95afc4be311277173b7c
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-26 15:25:10 +0500

    Middleware jwt authentication added

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -1,5 +1,13 @@
+import { response } from "express";
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.jwt;
+  if (!token)
+    return res.status(401).json({ message: "You are not authenticated" });
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid" });
+    req.userId = payload.userId;
+    next();
+  });
 };
