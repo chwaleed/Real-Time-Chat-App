dabde126e897e99bf07e4293ee0632cf44670416
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-27 15:46:06 +0500

    Route added in Constatnts

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,3 +4,4 @@ export const AUTH_ROUTES = "/api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const SIGNIN_ROUTE = `${AUTH_ROUTES}/signin`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
+export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
