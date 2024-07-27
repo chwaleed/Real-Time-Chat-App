b4fe1ea72cfb8d44ee94341e4cbb4ce193789709
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-27 16:49:31 +0500

    handlefilechagne fuction added

--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -3,13 +3,16 @@ import { Avatar } from "@/components/ui/avatar";
 import { colors, getColor } from "@/lib/utils";
 import { useAppStore } from "@/store/index.js";
 import { AvatarImage } from "@radix-ui/react-avatar";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoArrowBack } from "react-icons/io5";
 import { FaTrash, FaPlus } from "react-icons/fa";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import {
+  ADD_PROFILE_IMAGE_ROUTE,
+  UPDATE_PROFILE_ROUTE,
+} from "@/utils/constants";
 import { apiClient } from "@/lib/api-client";
 import { useNavigate } from "react-router-dom";
 
@@ -21,6 +24,7 @@ function Profile() {
   const [image, setImage] = useState("");
   const [hovered, setHovered] = useState("");
   const [selectedColor, setSelectedColor] = useState("");
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     if (userInfo.profileSetup) {
@@ -70,7 +74,29 @@ function Profile() {
       toast.error("Please setup profile.");
     }
   };
-
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+  const handleImageChange = async (event) => {
+    const file = event.target.file(0);
+    if (file) {
+      const formData = new FormData();
+      formData.append("profile-image", file);
+      const response = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE, formData, {
+        withCredentials: true,
+      });
+      if (response.status === 200 && response.data.image) {
+        setUserInfo({ ...userInfo, image: response.data.image });
+        toast.success("Image upadated successfully");
+      }
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const handleDeleteImage = async (event) => {};
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10 ">
       <div className="flex flex-col gap-10 w-[80wv] md:w-max ">
@@ -103,7 +129,10 @@ function Profile() {
               )}
             </Avatar>
             {hovered && (
-              <div className="absolute inset-0 flex items-center justify-center bg-black opacity-50 rounded-full  ">
+              <div
+                className="absolute inset-0 flex items-center justify-center bg-black opacity-50 rounded-full  "
+                onClick={image ? handleDeleteImage : handleFileInputClick}
+              >
                 {image ? (
                   <FaTrash className="text-white text-3xl cursor-pointer" />
                 ) : (
@@ -111,6 +140,14 @@ function Profile() {
                 )}
               </div>
             )}
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={handleImageChange}
+              name="profile-image"
+              accept=".png, .jpg, .jpeg, .svg, .webp"
+            />
           </div>
           <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
             <div className="w-full">
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -5,3 +5,4 @@ export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const SIGNIN_ROUTE = `${AUTH_ROUTES}/signin`;
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`;
 export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`;
+export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
