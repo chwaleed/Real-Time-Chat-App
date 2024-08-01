c1c469179e91ad64a91efcb543edf3c6125e7b04
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-01 15:05:18 +0500

    react-emoji-picker added

--- frontend/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ frontend/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -1,5 +1,7 @@
 function MessageContainers() {
-  return <div>MessageContainers</div>;
+  return (
+    <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full"></div>
+  );
 }
 
 export default MessageContainers;
