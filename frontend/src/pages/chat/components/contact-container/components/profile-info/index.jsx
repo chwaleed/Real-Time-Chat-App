07b725e0c17803c0bc3d3d14738b607ba724f096
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-02 17:33:34 +0500

    tooltip added

--- frontend/src/pages/chat/components/contact-container/components/profile-info/index.jsx
+++ frontend/src/pages/chat/components/contact-container/components/profile-info/index.jsx
@@ -1,5 +1,60 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { useAppStore } from "@/store";
+import { getColor } from "@/lib/utils";
+import { HOST } from "@/utils/constants";
+import { FiEdit2 } from "react-icons/fi";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
 function ProfileInfo() {
-  return <div>ProfileInfo</div>;
+  const { userInfo } = useAppStore();
+  return (
+    <div className=" absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]">
+      <div className="flex gap-3 items-center justify-center">
+        <div className="w-12 h-12 relative">
+          <Avatar className="h-12 w-12  rounded-full overflow-hidden">
+            {userInfo.image ? (
+              <AvatarImage
+                src={`${HOST}/${userInfo.image}`}
+                alt="Profile"
+                className="object-cover w-full h-full bg-black"
+              />
+            ) : (
+              <div
+                className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                  userInfo.color
+                )} `}
+              >
+                {userInfo.firstName
+                  ? userInfo.firstName.split("").shift()
+                  : userInfo.email.split("").shift()}
+              </div>
+            )}
+          </Avatar>
+        </div>
+        <div>
+          {userInfo.firstName && userInfo.lastName
+            ? `${userInfo.firstName} ${userInfo.lastName}`
+            : ""}
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <FiEdit2 className="text-purple-500 text-xl font-medium" />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileInfo;
