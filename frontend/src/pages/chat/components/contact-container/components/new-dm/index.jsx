5ee70be3ef2b140df98b5608631e60aa92f50917
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-03 14:59:33 +0500

    shadcn dialoge and scroll-area added

--- frontend/src/pages/chat/components/contact-container/components/new-dm/index.jsx
+++ frontend/src/pages/chat/components/contact-container/components/new-dm/index.jsx
@@ -1,5 +1,54 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+
 function NewDm() {
-  return <></>;
+  const [opneNewContactModel, setOpenNewContactModel] = useState(false);
+  return (
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <FaPlus
+              onClick={() => setOpenNewContactModel(true)}
+              className="text-neutral-400 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer  transition-all duration-300"
+            />
+          </TooltipTrigger>
+          <TooltipContent className="bg-[#1c1b1e] border-none text-white mb-2 p-3">
+            Select New Contact
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <Dialog open={opneNewContactModel} onOpenChange={setOpenNewContactModel}>
+        <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
+          <DialogHeader>
+            <DialogTitle>Please select a contact</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div>
+            <Input
+              placeholder="Search Contacts"
+              className="rounded-lg p-6  bg-[#2c2e3b] border-none"
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 }
 
 export default NewDm;
