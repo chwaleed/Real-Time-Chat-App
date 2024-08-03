8281b2f8fbb60ffe419e3e35c1157729713d0493
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-03 15:07:50 +0500

    new-dm jsx completed

--- frontend/src/pages/chat/components/contact-container/components/new-dm/index.jsx
+++ frontend/src/pages/chat/components/contact-container/components/new-dm/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
   Tooltip,
   TooltipContent,
@@ -12,12 +13,17 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import Lottie from "react-lottie";
+import { animationDefaultOptions } from "@/lib/utils";
 
 function NewDm() {
   const [opneNewContactModel, setOpenNewContactModel] = useState(false);
+  const [searchedContacts, setSearchedContacts] = useState([]);
+
+  const searchContacts = async (searchTerm) => {};
+
   return (
     <>
       <TooltipProvider>
@@ -43,8 +49,25 @@ function NewDm() {
             <Input
               placeholder="Search Contacts"
               className="rounded-lg p-6  bg-[#2c2e3b] border-none"
+              onChange={(e) => searchContacts(e.target.value)}
             />
           </div>
+          {searchedContacts.length <= 0 && (
+            <div className="flex-1 md:bg-[#1c1d25] md:flex mt-5 flex-col justify-center items-center  duration-1000 transition-all">
+              <Lottie
+                isClickToPauseDisabled={true}
+                height={100}
+                width={100}
+                options={animationDefaultOptions}
+              />
+              <div className=" text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-2xl text-xl transition-all duration-300 text-center">
+                <h3 className="poppins-medium">
+                  Hi<span className="text-purple-500">!</span> Search new
+                  <span className="text-purple-500"> Contact. </span>
+                </h3>
+              </div>
+            </div>
+          )}
         </DialogContent>
       </Dialog>
     </>
--- frontend/src/pages/chat/components/empty-chat-container/index.jsx
+++ frontend/src/pages/chat/components/empty-chat-container/index.jsx
@@ -3,7 +3,7 @@ import Lottie from "react-lottie";
 
 function EmptyChatContainer() {
   return (
-    <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all">
+    <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col  justify-center items-center hidden duration-1000 transition-all">
       <Lottie
         isClickToPauseDisabled={true}
         height={200}
