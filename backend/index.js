82eb5c0c233479c44372144ada0977e0e03df620
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-05 11:21:39 +0500

    contact route constant added

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import authRoutes from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -30,6 +31,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactsRoutes);
 
 const server = app.listen(port, () => {
   console.log(`Server is listining at port ${port}`);
--- backend/routes/ContactRoutes.js
+++ backend/routes/ContactRoutes.js
@@ -5,3 +5,5 @@ import { searchContacts } from "../controllers/ContactControllers.js";
 const contactsRoutes = Router();
 
 contactsRoutes.post("/search", verifyToken, searchContacts);
+
+export default contactsRoutes;
