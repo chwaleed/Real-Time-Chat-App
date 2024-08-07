c31771bb319bd4d859547b169942d8189c41b6f5
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-07 19:09:11 +0500

    socket connection fuction complted

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import authRoutes from "./routes/AuthRoutes.js";
 import contactsRoutes from "./routes/ContactRoutes.js";
+import setupSocket from "./socket.js";
 
 dotenv.config();
 const app = express();
@@ -36,3 +37,5 @@ app.use("/api/contacts", contactsRoutes);
 const server = app.listen(port, () => {
   console.log(`Server is listining at port ${port}`);
 });
+
+setupSocket(server);
--- backend/socket.js
+++ backend/socket.js
@@ -1,3 +1,35 @@
-const setupSocket = (server) => {};
+import { Server as SocketIOServer } from "socket.io";
+const setupSocket = (server) => {
+  const io = new SocketIOServer(server, {
+    cors: {
+      origin: process.env.ORIGIN,
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  const disconnect = (socket) => {
+    console.log(`Client Disconnect: ${socket.id}`);
+    for (const [userId, socketId] of userSocketMap.entries()) {
+      if (socketId === socket.id) {
+        userSocketMap.delete(userId);
+        break;
+      }
+    }
+  };
+
+  const userSocketMap = Map();
+  io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+      userSocketMap.set(userId, socket.id);
+      console.log(`User connected ${userId} with socket ID: ${socket.id}`);
+    } else {
+      console.log("User ID not provided during connection");
+    }
+
+    socket.on("disconnect", () => disconnect());
+  });
+};
 
 export default setupSocket;
