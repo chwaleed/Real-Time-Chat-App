29c378e935e11e4ca961d6ee2ce704892fe0956a
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-08-17 12:32:17 +0500

    socket context for client created

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -1,3 +1,36 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+import { useAppStore } from "@/store";
+import { HOST } from "@/utils/constants";
 import { createContext, useContext, useEffect, useRef } from "react";
+import { io } from "socket.io-client";
 
-const socketContext = createContext(null);
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = (children) => {
+  const socket = useRef();
+  const { userInfo } = useAppStore();
+  useEffect(() => {
+    if (userInfo) {
+      socket.current = io(HOST, {
+        withCredentials: true,
+        query: { userId: userInfo.id },
+      });
+      socket.connect.on("connect", () => {
+        console.log("Connected to socket Server");
+      });
+    }
+    return () => {
+      socket.current.disconnect();
+    };
+  }, [userInfo]);
+  return (
+    <SocketContext.Provider value={socket.current}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
