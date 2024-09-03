42cf56d7299b781890310b146487c00c2db665cf
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-09-03 22:10:47 +0500

    comeback

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -21,23 +21,12 @@ export const SocketProvider = ({ children }) => {
         withCredentials: true,
         query: { userId: userInfo.id },
       });
-
       socket.current.on("connect", () => {
-        console.log("Connected successfuly");
+        console.log("Connected to socket server.");
       });
-      console.log(socket.current);
       return () => {
-        // Ensure socket.current is defined before calling disconnect
-        if (socket.current) {
-          socket.current.disconnect();
-          socket.current = null; // Clear the reference
-        }
-      };
-    } else {
-      if (socket.current) {
         socket.current.disconnect();
-        socket.current = null; // Clear the reference
-      }
+      };
     }
   }, [userInfo]);
   return (
